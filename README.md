eeb052b86dea3a99993a29130d6d90bdfbc81203
Ryhor-Karnatsevich <spamjamesmorgan@gmail.com>
2026-03-20 18:02:09 +0100

    cleaning is done

--- README.md
+++ README.md
@@ -4,13 +4,17 @@ DATASET SOURCE: https://www.kaggle.com/datasets/jacksoncrow/stock-market-dataset
 
 ## Project Review:
 
+### Date
+- Standardized date column
 
 ### Missing Values
 - Deleted 649 rows with missing values
 
 ### Duplicates
-- No duplicates found
+- Deleted all duplicates
 
 ### Logic test
 - Got 1264 invalid rows
 - Deleted it
+- A significant number of records had invalid Open prices equal to zero
+it has been treated as missing values and removed to ensure data integrity.
--- src/Data_prep/Data cleaning.py
+++ src/Data_prep/Data cleaning.py
@@ -19,23 +19,45 @@ df = pd.read_csv(path)
 
 ### Data Cleaning
 
+df["Date"] = pd.to_datetime(df["Date"])
+
 print(df[df.isnull().any(axis=1)])
 df = df.dropna()
 print(df.isnull().sum())
 
 dup = df.duplicated().sum()
 print(dup)
+df = df.drop_duplicates()
 
 df = df[
     (df["High"]>= df["Low"]) &
     (df["High"] >= df["Close"]) &
-    (df["Low"] <= df["Close"])
+    (df["Low"] <= df["Close"]) &
+    (df["Volume"] >= 0)
 ]
-
 print(len(df[df["High"] < df["Low"]]))
 print(len(df[df["High"] < df["Close"]]))
 print(len(df[df["Low"]>df["Close"]]))
 print(len(df[df["Volume"] < 0]))
+
+
+df = df[
+    (df["High"] > 0) &
+    (df["Low"] > 0) &
+    (df["Close"] > 0) &
+    (df["Open"] > 0)
+]
+print(len(df[df["Open"] == 0]))
+print(len(df[df["Close"] == 0]))
+print(len(df[df["High"] == 0]))
+print(len(df[df["Low"] == 0]))
+
+df = df.sort_values(["Ticker","Date"])
+df = df.reset_index(drop=True)
+
+df.to_csv(output_path, index=False)
+
+
 
 
 
