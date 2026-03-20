abd826ddd274da982d1dc3cbf14b179ed72157cf
Ryhor-Karnatsevich <spamjamesmorgan@gmail.com>
2026-03-20 17:27:45 +0100

    still cleaning

--- src/Data_prep/Data cleaning.py
+++ src/Data_prep/Data cleaning.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv(r"../../Data/test_data.csv")
+is_test = True
+if is_test:
+    path = r"../../Data/test_data.csv"
+    output_path = r"../../Data/test_data_cleaned.csv"
+else:
+    path = r"../../Data/all_stocks.csv"
+    output_path = r"../../Data/all_stocks_cleaned.csv"
+
+df = pd.read_csv(path)
 
 ### Basic check
 # print(df.head())
@@ -9,21 +17,27 @@ df = pd.read_csv(r"../../Data/test_data.csv")
 # print(df.info())
 # print(df.columns)
 
-### missing data
-# print(df[df.isnull().any(axis=1)])
-# df = df.dropna()
-# print(df.isnull().sum())
-#
-# dup = df.duplicated().sum()
-# print(dup)
+### Data Cleaning
+
+print(df[df.isnull().any(axis=1)])
+df = df.dropna()
+print(df.isnull().sum())
+
+dup = df.duplicated().sum()
+print(dup)
 
 df = df[
-    (df["High"]>= df["Low"]) & (df["High"] >= df["Close"]) & (df["Low"] <= df["Close"])
+    (df["High"]>= df["Low"]) &
+    (df["High"] >= df["Close"]) &
+    (df["Low"] <= df["Close"])
 ]
+
 print(len(df[df["High"] < df["Low"]]))
 print(len(df[df["High"] < df["Close"]]))
 print(len(df[df["Low"]>df["Close"]]))
 print(len(df[df["Volume"] < 0]))
+
+
 
 
 
