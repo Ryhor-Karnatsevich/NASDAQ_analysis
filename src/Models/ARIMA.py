435012f5e4095948a7727e58e3c83d00c8980c53
Ryhor-Karnatsevich <spamjamesmorgan@gmail.com>
2026-03-25 15:45:27 +0100

    ARIMA model in progress

--- src/Models/ARIMA.py
+++ src/Models/ARIMA.py
@@ -13,34 +13,85 @@ else:
 df = pd.read_csv(path)
 df["Date"] = pd.to_datetime(df["Date"])
 df = df.sort_values(["Ticker", "Date"])
+#Filter
+min_obs = 1000
+df = df.groupby("Ticker").filter(lambda x: len(x) >= min_obs)
+# Seed Setup
+np.random.seed(52)
 
-ticker = 'AAPL'
-data = df[df['Ticker'] == ticker].copy()
-data = data.sort_values("Date")
-data = data.set_index("Date")
 
-data = data.asfreq("B")
-data = data.ffill()
+### ARIMA SETUP
+split_date = "2019-01-01"
+order = (1,0,0)
+use_random = True
+n = 10
 
-# TRAIN / TEST SPLIT
-train = data[data.index < "2019-01-01"]["Returns"]
-test = data[data.index >= "2019-01-01"]["Returns"]
-print("Train size:", len(train))
-print("Test size:", len(test))
+if use_random:
+    tickers = np.random.choice(df["Ticker"].unique(), size=n, replace=False)
+else:
+    tickers = ["AAPL", "NVDA"]
 
-model  = ARIMA(train,  order=(1,0,0))
-result = model.fit()
+### Model Function
+def model_run(df,ticker,order,split_date, verbose=True):
+    data = df[df['Ticker'] == ticker].copy()
+    data = data.sort_values("Date")
+    data = data.set_index("Date")
 
-print(result.summary())
+    data = data.asfreq("B")
+    data = data.ffill()
+#-----------------------------------------------------------
+    train = data[data.index < split_date]["Returns"]
+    test = data[data.index >= split_date]["Returns"]
+    print(f"Ticker: {ticker}")
+    print("Train size:", len(train))
+    print("Test size:", len(test))
+# Additional Length filtrating
+    if len(train) < 750:
+        return None
+#-----------------------------------------------------------
+    model  = ARIMA(train,  order=order)
+    result = model.fit()
+    if verbose:
+        print(result.summary())
 
 # Forecast
-forecast = result.forecast(steps=len(test))
+    forecast = result.forecast(steps=len(test))
 
 # METRICS
-mse = mean_squared_error(test, forecast)
+    mse = mean_squared_error(test, forecast)
+    direction = np.mean(np.sign(forecast) == np.sign(test))
 
-direction = np.mean(np.sign(forecast) == np.sign(test))
+    print("MSE:", mse)
+    print("Direction:", direction)
 
-print("MSE:", mse)
-print("Direction:", direction)
+    return {
+        "ticker": ticker,
+        "ar_coef": result.params.get("ar.L1", np.nan),
+        "p_value": result.pvalues.get("ar.L1", np.nan),
+        "mse": mse,
+        "direction": direction,
+        "train size": len(train),
+        "test size": len(test)
+
+    }
+
+#---------------------------------------------------------------
+results = []
+
+for ticker in tickers:
+    r = model_run(df, ticker, order,split_date)
+    if r is not None:
+        results.append(r)
+
+results_df = pd.DataFrame(results)
+
+# Adding average row
+#--------------------------------------------------------------------------------------
+avg_row = results_df.mean(numeric_only=True)
+avg_row["ticker"] = "AVG"
+results_df = pd.concat([results_df, pd.DataFrame([avg_row])], ignore_index=True)
+#--------------------------------------------------------------------------------------
+
+print(results_df.round(4))
+
 
